20210f8fdeba91997014307f30aa2836f73c9307
E Bolton <eebmagic@gmail.com>
2022-05-07 17:50:19 -0400

    working keyboard example in tests

--- tests/key.py
+++ tests/key.py
@@ -1,13 +1,51 @@
-import keyboard
+from sys import exit
+import pygame
+from pygame.locals import *
+pygame.init()
 
-while True:
-    if keyboard.read_key() == "p":
-        print("You pressed p")
-        break
+white = 255, 255, 255
 
-while True:
-    if keyboard.is_pressed("q"):
-        print("You pressed q")
-        break
-        
-keyboard.on_press_key("r", lambda _:print("You pressed r"))
+size = screen_width, screen_height = 600, 400
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+
+def game_loop():
+    fps_cap = 120
+    running = True
+    while running:
+        clock.tick(fps_cap)
+
+        for event in pygame.event.get():    # error is here
+            if event.type == pygame.QUIT:
+                running = False
+
+        keys = pygame.key.get_pressed()
+        values = {
+            'q': keys[K_q],
+            'w': keys[K_w],
+            'e': keys[K_e],
+            'r': keys[K_r],
+            'a': keys[K_a],
+            's': keys[K_s],
+            'd': keys[K_d],
+            'f': keys[K_f],
+            'z': keys[K_z],
+            'x': keys[K_x],
+            'c': keys[K_c],
+            'v': keys[K_v],
+            '1': keys[K_1],
+            '2': keys[K_2],
+            '3': keys[K_3],
+            '4': keys[K_4],
+        }
+        print(values)
+
+        screen.fill(white)
+
+        pygame.display.flip()
+
+    pygame.quit()
+    exit()
+
+game_loop()
+#!/usr/bin/env python
